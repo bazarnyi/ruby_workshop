815aa29d5197f89a808e117dc27dc5b4e00d2872
Denys Bazarnyi <dbaza@softserveinc.com>
2018-09-12 16:19:47 +0300

    New step added

--- spec/features/first_spec.rb
+++ spec/features/first_spec.rb
@@ -1,7 +1,9 @@
 feature 'Visit wikipedia page', js: true do
   scenario 'Unreggistered visitor visits home page' do
     visit 'https://en.wikipedia.org/'
-
     expect(page).to have_content 'Welcome to Wikipedia'
+
+    find(:xpath, '//*[@id="mp-topbanner"]/div/div[1]/a').click
+    expect(page).to have_content 'From Wikipedia, the free encyclopedia'
   end
 end
